9dd38594c5eb51cb43eab1f8610b24996610459d
Stina Renberg <stinarenberg1@gmail.com>
2020-10-15 15:32:31 +0200

    commit of javaprog

---
+++ Exercise01.java
@@ -0,0 +1,11 @@
+public class Exercise01 {
+  public static void main(String[] args){
+	int a = 10;
+	int b = 20;
+		System.out.println("Värde int a: " + a + "\n" + "Värde int b: " + b);
+	a = a+b;
+	b = a-b;
+	a = a-b;
+		System.out.println("Int a's nya värde: " + a + "\n" + "int b's nya värde är: " + b);	
+  }
+}
